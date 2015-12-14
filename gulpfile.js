b7dd51712e3402b296c4f1832dc945f6516dfa14
SrTobi <code.databyte@gmail.com>
2015-12-14 14:22:36 +0100

    fixed sourcemap problem for subdirectiories. closes #8.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var ts = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
+var path = require('path');
 
+function pathBack(path) {
+  return path.replace(/[^\/]+/g, "..");
+}
 
 var tsProject = ts.createProject('./tsconfig.json', { sortOutput: true });
 
@@ -14,6 +18,12 @@ gulp.task('build', function() {
     .pipe(sourcemaps.init())
       .pipe(ts(tsProject)).js
       .pipe(babel())
-    .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '../../src'}))
+    .pipe(sourcemaps.write('.', {includeContent: false,
+      sourceRoot: function(file) {
+        var relMapPath = path.dirname(file.sourceMap.sources[0]);
+        var relBackPath = pathBack(relMapPath);
+        return path.join("..", relBackPath).replace(/\\/g, "/");
+      }
+    }))
     .pipe(gulp.dest('out'));
 });
