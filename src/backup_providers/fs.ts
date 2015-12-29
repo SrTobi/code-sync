19168dd0e71c1009169503a9039c3988b5692569
SrTobi <code.databyte@gmail.com>
2015-12-29 21:14:19 +0100

    implemented getConfigs for FSBackupProvider

--- src/backup_providers/fs.ts
+++ src/backup_providers/fs.ts
@@ -2,6 +2,7 @@ import {BackupProvider, BackupProviderConfigTemplate} from '../backup_provider';
 import {ConfigHandle, FileConfigHandle} from '../config_handle';
 import {ConfigProviderTask, ConfigProviderBackend} from '../config_provider';
 import * as path from 'path';
+import * as utils from '../utils';
 
 interface FSBackupProviderConfigTemplate extends BackupProviderConfigTemplate{
 	path: string;
@@ -17,12 +18,23 @@ export class FSBackupProvider implements BackupProvider {
 		this._basepath = t.path;
 	}
 	
-	getConfigs(): Promise<ConfigHandle[]> {
-		throw new Error("Not implemented");
+	async getConfigs(): Promise<ConfigHandle[]> {
+        let configs: ConfigHandle[] = [];
+		let mountpoints = await utils.glob("*/", {cwd: this._basepath});
+        for(let mp of mountpoints) {
+            mp = mp.replace(/[\/\\]/g, "");
+            let p = path.join(this._basepath, mp);
+            let files = await utils.glob(path.join(p, "**/*.*"), {cwd: p});
+            for(let file of files) {
+                configs.push(new FileConfigHandle(file, mp, this, this._backend));
+            }
+        }
+        
+        return configs;
 	}
 	
 	getConfig(template: ConfigHandle): Promise<ConfigHandle> {
-		return new Promise(resolve => {
+		return new Promise<ConfigHandle>(resolve => {
 			let mp = template.mountpoint();
 			let name = template.name();
 			let filepath = path.join(this._basepath, mp, name);
